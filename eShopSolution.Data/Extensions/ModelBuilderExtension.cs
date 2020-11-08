cc1db0a7a88782da3ef0ffbbf9db0b3d9421d615
vannlse130680 <nguyenlamvan1999@gmail.com>
2020-11-08 09:47:24 +0700

    data seeding

---
+++ eShopSolution.Data/Extensions/ModelBuilderExtension.cs
@@ -0,0 +1,131 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Extensions
+{
+    public static class ModelBuilderExtension
+    {
+       public static void Seed (this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppConfig>().HasData(
+                new AppConfig() { Key = "HomeTitle", Value = "This is home page of eShopSolution" },
+                new AppConfig() { Key = "HomeKeyWord", Value = "This is keyword  of eShopSolution" },
+                new AppConfig() { Key = "HomeDescription", Value = "This is description  of eShopSolution" });
+
+            modelBuilder.Entity<Language>().HasData(
+                new Language() { Id = "vi-VN", Name = "Tiếng Việt", IsDefault = true },
+                new Language() { Id = "en-US", Name = "English", IsDefault = false });
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category()
+                {
+                    Id = 1,
+                    IsShowHome = true,
+                    ParentId = null,
+                    SortOrder = 1,
+                    Status = Enums.Status.Active,
+                    
+                },
+                new Category()
+                {
+                    Id = 2,
+                    IsShowHome = true,
+                    ParentId = null,
+                    SortOrder = 2,
+                    Status = Enums.Status.Active,
+                    
+                });
+
+            modelBuilder.Entity<CategoryTranslation>().HasData(
+                new CategoryTranslation()
+                {
+                    Id = 1,                    
+
+                    CategoryId = 1,
+                    Name = "Áo nam",
+                    LanguageId = "vi-VN",
+                    SeoAlias = "ao-nam",
+                    SeoDescription = "Sản phẩm thời trang nam",
+                    SeoTitle = "Sản phẩm thời trang nam"
+                },
+                new CategoryTranslation()
+                {
+                    Id= 2,
+                    CategoryId = 1,
+                    Name = "Men Shirt",
+                    LanguageId = "en-US",
+                    SeoAlias = "men-shirt",
+                    SeoDescription = "The Shirt products for men",
+                    SeoTitle = "The Shirt products for men"
+               },
+               new CategoryTranslation()
+               {
+                   Id = 3,
+                   CategoryId = 2,
+                   Name = "Áo nữ",
+                   LanguageId = "vi-VN",
+                   SeoAlias = "ao-nu",
+                   SeoDescription = "Sản phẩm thời trang nữ",
+                   SeoTitle = "Sản phẩm thời trang nữ"
+               },
+               new CategoryTranslation()
+               {
+                   Id = 4,
+                   CategoryId = 2,
+                   Name = "Áo nữ",
+                   LanguageId = "vi-VN",
+                   SeoAlias = "ao-nu",
+                   SeoDescription = "Sản phẩm thời trang nữ",
+                   SeoTitle = "Sản phẩm thời trang nữ"
+               });
+            modelBuilder.Entity<Product>().HasData(
+                new Product()
+                {
+                    Id = 1,
+                    DateCreated = DateTime.Now,
+                    OrginalPrice = 100000,
+                    Price = 20000,
+                    Stock = 0,
+                    ViewCount = 0,
+                  
+                    
+                });
+            modelBuilder.Entity<ProductTranslation>().HasData(
+                    new ProductTranslation()
+                    {
+                        Id =1,
+                        ProductId = 1,
+                        Name = "Áo sơ mi nam trắng Việt Tiến",
+                        LanguageId = "vi-VN",
+                        SeoAlias = "ao-so-mi-nam-viet-tien",
+                        SeoDescription = "Áo sơ mi nam trắng Việt Tiến",
+                        SeoTitle = "Áo sơ mi nam trắng Việt Tiến",
+                        Details = "Áo sơ mi nam trắng Việt Tiến",
+                        Description = "Áo sơ mi nam trắng Việt Tiến"
+                    },
+                    new ProductTranslation()
+                    {
+                        Id = 2,
+                        ProductId = 1,
+                        Name = "Viet Tien Men white T-Shirt",
+                        LanguageId = "en-US",
+                        SeoAlias = "Viet Tien Men white T-Shirt",
+                        SeoDescription = "Viet Tien Men white T-Shirt",
+                        SeoTitle = "Viet Tien Men white T-Shirt",
+                        Details = "Viet Tien Men white T-Shirt",
+                        Description = "Viet Tien Men white T-Shirt"
+                    }) ;
+            modelBuilder.Entity<ProductInCategory>().HasData(
+                    new ProductInCategory()
+                    {
+                        ProductId = 1,
+                        CategoryId = 1
+                        
+                    });
+
+        }
+    }
+}
---
+++ eShopSolution.Data/Migrations/20201107052813_9.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class _9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SeoAlias",
+                table: "Products");
+
+            migrationBuilder.InsertData(
+                table: "AppConfigs",
+                columns: new[] { "Key", "Value" },
+                values: new object[,]
+                {
+                    { "HomeTitle", "This is home page of eShopSolution" },
+                    { "HomeKeyWord", "This is keyword  of eShopSolution" },
+                    { "HomeDescription", "This is description  of eShopSolution" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "IsShowHome", "ParentId", "SortOrder", "Status" },
+                values: new object[,]
+                {
+                    { 1, true, null, 1, 1 },
+                    { 2, true, null, 2, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Languages",
+                columns: new[] { "Id", "IsDefault", "Name" },
+                values: new object[,]
+                {
+                    { "vi-VN", true, "Tiếng Việt" },
+                    { "en-US", false, "English" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "DateCreated", "OrginalPrice", "Price" },
+                values: new object[] { 1, new DateTime(2020, 11, 7, 12, 28, 12, 147, DateTimeKind.Local).AddTicks(1471), 100000m, 20000m });
+
+            migrationBuilder.InsertData(
+                table: "CategoryTranslations",
+                columns: new[] { "Id", "CategoryId", "LanguageId", "Name", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, 1, "vi-VN", "Áo nam", "ao-nam", "Sản phẩm thời trang nam", "Sản phẩm thời trang nam" },
+                    { 3, 2, "vi-VN", "Áo nữ", "ao-nu", "Sản phẩm thời trang nữ", "Sản phẩm thời trang nữ" },
+                    { 4, 2, "vi-VN", "Áo nữ", "ao-nu", "Sản phẩm thời trang nữ", "Sản phẩm thời trang nữ" },
+                    { 2, 1, "en-US", "Men Shirt", "men-shirt", "The Shirt products for men", "The Shirt products for men" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductInCategories",
+                columns: new[] { "CategoryId", "ProductId" },
+                values: new object[] { 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "ProductTranslations",
+                columns: new[] { "Id", "Description", "Details", "LanguageId", "Name", "ProductId", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, "Áo sơ mi nam trắng Việt Tiến", "Áo sơ mi nam trắng Việt Tiến", "vi-VN", "Áo sơ mi nam trắng Việt Tiến", 1, "ao-so-mi-nam-viet-tien", "Áo sơ mi nam trắng Việt Tiến", "Áo sơ mi nam trắng Việt Tiến" },
+                    { 2, "Viet Tien Men white T-Shirt", "Viet Tien Men white T-Shirt", "en-US", "Viet Tien Men white T-Shirt", 1, "Viet Tien Men white T-Shirt", "Viet Tien Men white T-Shirt", "Viet Tien Men white T-Shirt" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeDescription");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeKeyWord");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeTitle");
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ProductInCategories",
+                keyColumns: new[] { "CategoryId", "ProductId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "en-US");
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "vi-VN");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SeoAlias",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
